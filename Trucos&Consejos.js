dc8cd8d0eed42398b23d4edeb1a9c71a56a937ef
Sol <solzito.1889@gmail.com>
2024-05-01 13:35:35 -0300

    se agregan clases para el estilado y data para las cards

--- Trucos&Consejos.js
+++ Trucos&Consejos.js
@@ -8,26 +8,26 @@ let trucos = [
     descripcionCorta:
       "Como expertos viajeros, somos conscientes de que planificar unas vacaciones puede tomar bastante tiempo...",
     consejos: [
-      "Determinen su presupuesto",
-      "Guarden sus resultados favoritos para más tarde",
-      "Dividan las tareas",
-      "Utilicen aplicaciones para organizarse",
-      "Guarden toda la información de su viaje en un solo lugar",
+      "Determinen su presupuesto.",
+      "Guarden sus resultados favoritos para más tarde.",
+      "Dividan las tareas.",
+      "Utilicen aplicaciones para organizarse.",
+      "Guarden toda la información de su viaje en un solo lugar.",
     ],
   },
   {
     id: 2,
-    image: "../img/preparacionViaje.jpg",
-    titulo: "4 consejos para prepararse para un viaje futuro",
+    image: `../img/presupuesto.jpg`,
+    titulo: "Presupuesto y dinero para viajar",
     descripcion:
-      "La idea de viajar puede parecer muy lejana. Por eso estamos acá, para ayudarte a conseguir la información que necesitás para que puedas soñar ahora y viajar después. Como viajeros que somos, tenemos ganas de desempolvar nuestros pasaportes y volver a viajar. Sin embargo, hasta que llegue ese día, dedicamos nuestra energía a prepararnos para ese increíble viaje futuro.",
+      "Que el dinero no sea un obstáculo: hay muchas maneras de autofinanciar un viaje.",
     descripcionCorta:
-      "La idea de viajar puede parecer muy lejana. Por eso estamos acá, para ayudarte a conseguir la información que necesitás para que puedas soñar ahora y viajar...",
+      "Que el dinero no sea un obstáculo: hay muchas maneras de autofinanciar un viaje.",
     consejos: [
-      "Fortalecé el pensamiento positivo",
-      "Prepará tu presupuesto",
-      "Seguí los precios",
-      "Escapate a mundos imaginarios",
+      "Haz un presupuesto de gastos estimados de cada destino..",
+      "Hazte con una tarjeta que NO cobre comisiones.",
+      "Descargate una APP para saber cuál es el cambio de moneda exacto.",
+      "No tengas todo el dinero en un sitio solo: deja algo de dinero de emergencia aparte (por ejemplo en la mochila/dentro un libro…).",
     ],
   },
   {
@@ -39,42 +39,87 @@ let trucos = [
     descripcionCorta:
       "Podríamos buscar miles de cosas para hacer, pero si viajamos con la mente abierta, vamos a encontrar un millón de cosas para hacer...",
     consejos: [
-      "Jamás salgo a recorrer con auriculares(por seguridad y una experiencia que tuve, me gusta ser consciente de lo que pasa a mi alrededor)",
-      "Viajar con la mente abierta",
-      "Reservar un tiempo",
-      "Bajarse el mapa de Google con anticipación",
+      "Jamás salgo a recorrer con auriculares.",
+      "Viajar con la mente abierta.",
+      "Reservar un tiempo.",
+      "Bajarse el mapa de Google con anticipación.",
     ],
   },
   {
     id: 4,
     image: "../img/hacerAmigos.jpg",
-    titulo: "5 consejos para hacer amigos en tus viajes",
+    titulo: "Consejos para hacer amigos en tus viajes",
     descripcion:
       "Un viaje en solitario te puede revolucionar la vida. Cuando no dependés de nadie, podés hacer lo que quieras, cuando quieras y como quieras. Sin embargo, esa sensación de libertad a veces cansa y puede ser que la soledad te sorprenda incluso en el paisaje más maravilloso. En un viaje sin más compañía que tus propios pensamientos, siempre llega un momento en que apetece socializar.",
     descripcionCorta:
       "Un viaje en solitario te puede revolucionar la vida. Cuando no dependés de nadie, podés hacer lo que quieras, cuando quieras y como quieras...",
     consejos: [
-      "Unite a un tour gratuito a pie por la ciudad",
-      "Usá internet para conocer a gente interesante",
-      "Aprendé algo nuevo mientras hacés amigos",
-      "Sacale partido a Meetup",
-      "Reservá tu lugar en una excursión organizada y lanzate a la aventura",
+      "Unite a un tour gratuito a pie por la ciudad.",
+      "Usá internet para conocer a gente interesante.",
+      "Aprendé algo nuevo mientras hacés amigos.",
+      "Sacale partido a Meetup.",
+      "Reservá tu lugar en una excursión organizada y lanzate a la aventura.",
     ],
   },
   {
     id: 5,
-    image: `../img/viajesConAmigos.jpg`,
-    titulo: "Cómo organizar un viaje con amigos",
+    image: `../img/amigos.jpg`,
+    titulo: "Organizar un viaje con amigos",
     descripcion:
       "Como expertos viajeros, somos conscientes de que planificar unas vacaciones puede tomar bastante tiempo. Esto es aún más complicado si vas con amigos y tienen que ponerse todos de acuerdo. Queremos facilitarles este proceso y para ello, les acompañamos desde el momento en el que tienen que decidir su próximo destino",
     descripcionCorta:
       "Como expertos viajeros, somos conscientes de que planificar unas vacaciones puede tomar bastante tiempo...",
     consejos: [
-      "Determinen su presupuesto",
-      "Guarden sus resultados favoritos para más tarde",
-      "Dividan las tareas",
-      "Utilicen aplicaciones para organizarse",
-      "Guarden toda la información de su viaje en un solo lugar",
+      "Determinen su presupuesto.",
+      "Guarden sus resultados favoritos para más tarde.",
+      "Dividan las tareas.",
+      "Utilicen aplicaciones para organizarse.",
+      "Guarden toda la información de su viaje en un solo lugar.",
+    ],
+  },
+  {
+    id: 6,
+    image: "../img/preparacionViaje.jpg",
+    titulo: "4 consejos para un viaje futuro",
+    descripcion:
+      "La idea de viajar puede parecer muy lejana. Por eso estamos acá, para ayudarte a conseguir la información que necesitás para que puedas soñar ahora y viajar después. Como viajeros que somos, tenemos ganas de desempolvar nuestros pasaportes y volver a viajar. Sin embargo, hasta que llegue ese día, dedicamos nuestra energía a prepararnos para ese increíble viaje futuro.",
+    descripcionCorta:
+      "La idea de viajar puede parecer muy lejana. Estamos acá, para ayudarte a conseguir la información necesaria para que puedas soñar y viajar...",
+    consejos: [
+      "Fortalecé el pensamiento positivo.",
+      "Prepará tu presupuesto.",
+      "Seguí los precios.",
+      "Escapate a mundos imaginarios.",
+    ],
+  },
+  {
+    id: 7,
+    image: `../img/salud.jpg`,
+    titulo: "Lo mas importante, la salud",
+    descripcion:
+      "Al viajar solemos ir y querer recorrer todo, probar y hacer cosas fuera de lo habitual, por eso, esta bueo que seamos concientes de nuestra salud.",
+    descripcionCorta:
+      "Al viajar solemos querer recorrer todo, probar y hacer cosas fuera de lo habitual, por eso, debemos ser concientes de la salud...",
+    consejos: [
+      "Lleva un pequeño botiquín y no una farmacia.",
+      "Vacúnate cuando sea necesario y asegúrate.",
+      "Lleva alochol en gel, no viene mal.",
+      "Siempre lleva una botella de agua.",
+      "Chocolates o snack",
+    ],
+  },
+  {
+    id: 8,
+    image: `../img/mochila-equipaje.jpg`,
+    titulo: "La mochila/equipaje",
+    descripcion:
+      "Siempre lleva equipaje ligero, si no quieres acabar tu viaje con la misma columna vertebral que el jorobado de Notre Dame aprenderás rápido…Lleva una mochila extra. Pequeña. Para excursiones y paseos por la ciudad",
+    descripcionCorta:
+      "Siempre lleva equipaje ligero, si no quieres acabar tu viaje con la misma columna vertebral que el jorobado de Notre Dame...",
+    consejos: [
+      "Lleva una mochila extra, para excursiones y paseos.",
+      "No te olvides un candado.",
+      "El Kindle se ha transformado en todo un imprescindible de nuestra mochila.",
     ],
   },
 ];
@@ -82,11 +127,12 @@ let trucos = [
 const cards = document.querySelector("#containerCards");
 
 for (data of trucos) {
-  cards.innerHTML += `<div class="col-sm"> <div class="card border" style="width: 20rem; height: max-content">
+  cards.innerHTML += `<div class="col-sm"> <div class="card border" style="width: 20rem; height: 30rem">
    <img
      class="img-thumbnail"
      src=${data.image}
      alt=${data.titulo}
+     style= "height: 15rem";
    />
    <div class="card-body">
      <h5 class="card-title">${data.titulo}</h5>
@@ -95,22 +141,20 @@ for (data of trucos) {
      </p>
       <div>
         <p>
-          <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${
+          <button class="btn btn-primary btn-consejos" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${
             data.id
           }" aria-expanded="false" aria-controls="collapse${data.id}">
             Consejos:
           </button>
         </p>
-        <div class="collapse" id="collapse${data.id}">
-          <div class="card card-body">
-          <ul>
+        <div class="collapse conteiner-consejos" id="collapse${data.id}">
+          <ul class="conteiner-consejos">
           ${data.consejos.map((el) => `<li>${el}</li>`)}
           </ul>
-          </div>
         </div>
      </div>
    </div>
    </div>
    </div>
-   </br>`;
+   `;
 }
